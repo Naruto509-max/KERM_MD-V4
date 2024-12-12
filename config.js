0d39793e0f1f8c3a7e40398e7338a33e4719f7ac
Naruto509-max <dmommy038@gmail.com>
2024-12-12 08:51:55 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "KZZX2QQQ#8PDRU6aX_DkwQr5bCD5XPC7kx8BuKe6Ktq841vlRggw",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
